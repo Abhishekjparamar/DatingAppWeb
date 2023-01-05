326b157ce1db9346d44253fada601f4cbc2258b8
AbhishekJparmar <abhishekj.parmar@nexuslinkservices.in>
2023-01-05 12:51:26 +0530

    Add project files.

---
+++ DatingAppWeb/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingAppWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("GetUsers")]
+        public async Task<ActionResult<List<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        [HttpGet]
+        [Route("GetAllUserById/{Id}")]
+        public async Task<ActionResult<AppUser>> GetAllUserById(int Id)
+        {
+            return await _context.Users.FindAsync(Id);
+        }
+
+    }
+}
